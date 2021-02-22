ae1899049014142735ec60828483dfbc81915ca0
Joe Wickes <joe.c.wickes@gmail.com>
2021-02-22 15:43:14 -0500

    Finish styling

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import HeadService from './../../services/head-service';
 import GuessService from './../../services/guess-service';
 
+import './LearningRoute.css';
+
 class LearningRoute extends Component {
   
   state = {
@@ -73,32 +75,46 @@ class LearningRoute extends Component {
     }
 
     const guessPrompt = (
-      <section>
-        <h2>Translate the word:</h2>
-        <span>{this.state.word}</span>
-        <p>Your total score is: {this.state.totalScore}</p>
-        <form onSubmit={(e) => handleGuessSubmit(e)}>
-          <label htmlFor="learn-guess-input">What's the translation for this word?</label>
-          <input id="learn-guess-input" type="text" onChange={(e) => updateGuess(e)} required />
-          <button type="submit">Submit your answer</button>
-        </form>
-        <p>You have answered this word correctly {this.state.correctCount} times.</p>
-        <p>You have answered this word incorrectly {this.state.incorrectCount} times.</p>
+      <section className="LearningRoute">
+        <div className="guessPrompt">
+          <h2>Translate the word:</h2>
+          <span>{this.state.word}</span>
+          <div className='total-score'>
+            <p>Your total score is: {this.state.totalScore}</p>
+          </div>
+          <form onSubmit={(e) => handleGuessSubmit(e)}>
+            <label htmlFor="learn-guess-input">What's the translation for this word?</label>
+            <input className="input-box" id="learn-guess-input" type="text" onChange={(e) => updateGuess(e)} required />
+            <div>
+              <button className="regular-button" type="submit">Submit your answer</button>
+            </div>
+          </form>
+          <div className="correct-count">
+            <p>You have answered this word correctly {this.state.correctCount} times.</p>
+          </div>
+          <div class="incorrect-count">
+            <p>You have answered this word incorrectly {this.state.incorrectCount} times.</p>
+          </div>
+        </div>
       </section>
     )
 
     const resultsPrompt = (
-      <>
-        <h2>{this.state.isCorrect === false ? 'Good try, but not quite right :(' : null}</h2>
-        <h2>{this.state.isCorrect ? 'You were correct! :D' : null}</h2>
-        <div className="DisplayFeedback">
-          <p>{`The correct translation for ${this.state.word} was ${this.state.translation} and you chose ${this.state.guess}!`}</p>
+      <section className="LearningRoute">
+        <div className="resultsPrompt">
+          <h2>{this.state.isCorrect === false ? 'Good try, but not quite right :(' : null}</h2>
+          <h2>{this.state.isCorrect ? 'You were correct! :D' : null}</h2>
+          <div className="DisplayFeedback">
+            <p>{`The correct translation for ${this.state.word} was ${this.state.translation} and you chose ${this.state.guess}!`}</p>
+          </div>
+          <div className="DisplayScore">
+            <p>{`Your total score is: ${this.state.totalScore}`}</p>
+          </div>
+          <div className="results-button-container">
+            <button className="regular-button" onClick={(e) => handleTryAnother(e)}>Try another word!</button>
+          </div>
         </div>
-        <div className="DisplayScore">
-          <p>{`Your total score is: ${this.state.totalScore}`}</p>
-        </div>
-        <button onClick={(e) => handleTryAnother(e)}>Try another word!</button>
-      </>
+      </section>
     );
 
     const handlePrompts = () => {
